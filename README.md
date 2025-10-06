aa340fea23c519de7023391f2e19b70befe4ef97
CipherYuvraj <ronak41924@gmail.com>
2025-10-06 20:18:33 +0530

    empty

--- README.md
+++
@@ -1,2 +0,0 @@
-# NeuroTask
-NeuroTask is a sleek and intelligent to-do web app designed to boost productivity and streamline task management. With smart prioritization and a distraction-free interface, it helps you stay organized and get things done effortlessly. 
--- frontend/src/components/CreateTodo.jsx
+++
@@ -1,8 +0,0 @@
-// import tailwind from 'tailwindcss';
-export function CreateTodo(){
-    return <div>
-        <input className="flex items-center justify-center min-h-screen bg-gray-200" type="text" placeholder="title" /><br />
-        <input type="text" placeholder="description"/><br />
-        <button>Add a todo</button>
-    </div>
-}
